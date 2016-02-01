bbedb25b0c06a9580b53f1d10ee9521225a33dab
DerevenetsArtyom <fenix7036@gmail.com>
2016-02-01 21:51:58 +0200

    Add some comments to func

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
-from .forms	import FormForPost
+from .forms import FormForPost
 
 
 def post_list(request):
@@ -10,11 +12,13 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+# Show only single post.Else - 404
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+# For creating new post
 def post_new(request):
     if request.method == "POST":
         form = FormForPost(request.POST)
@@ -28,6 +32,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+# For correcting post
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.method == "POST":
